7457b6ead0fce53ccfd745b041769bf30d1bd8e4
Tor Andre Pettersen <totto.varoze@gmail.com>
2021-09-16 00:19:44 +0200

    idk what happened but here's the real commit

--- Compulsory 1/Source.cpp
+++ Compulsory 1/Source.cpp
@@ -10,11 +10,14 @@ void PlayGameAI();
 
 const int rows{ 3 };
 const int cols{ 3 };
-char Board[rows][cols]{}; // Storage for the different squares
-int TurnNumber{ 1 }; // We start on Turn 1
-int PlayerNumber{ 1 }; // Player 1 starts
-int PlayerNumberChoice{}; // Letting the player choose whether to play as X or O against an AI
-char PlayerNumberChoiceCH{}; // It looks cleaner if we let the user answer 'x' or 'o' when we ask them who to play, so we store it in a char first
+char Board[rows][cols]{};   // Storage for the different squares
+int TurnNumber{ 1 };        // We start on Turn 1
+int PlayerNumber{ 1 };      // Player 1 starts
+int PlayerNumberChoice{};   /* Letting the player choose whether to play as X or O
+                            against an AI*/
+char PlayerCharacter{};     /* It looks cleaner if we let the user answer 'x'
+                            or 'o' when we ask them who to play, so we store it
+                            in a char first*/
 
 int main() {
 
@@ -22,8 +25,9 @@ int main() {
     bool RepeatGame{};
     bool RepeatQuestion{};
     int PlayerChoice{};
-    
-    srand(time(NULL)); // Set the random seed to current system time, for the "AI" to choose a random square
+
+    srand(time(NULL)); /* Set the random seed to current system time, for the "AI" to
+    choose a random square*/
     do {
         system("cls"); // Clear the screen of any potential previous game
         std::cout << "Hello, and welcome to tic-tac-toe!\n";
@@ -49,8 +53,6 @@ int main() {
         case 2:
             PlayGameAI(); // If they entered 2, play a game against an AI that chooses random squares
             break;
-        case 3:
-            // COMING SOON™
         default:
             std::cout << "Something went wrong, please notify the developers!\n";
             EXIT_FAILURE;
@@ -61,10 +63,13 @@ int main() {
         }
         do {
             // Ask if the game should be repeated, and set RepeatGame accordingly
-            std::cout << "Would you like to play again?\n"; // No need to tell them to use Y/N format, since we store the answer in a char variable, it will only take the first letter anyway
+            std::cout << "Would you like to play again?\n"; /* No need to tell them to use Y / N format, since
+                                                            we store the answer in a char variable, it will only
+                                                            take the first letter anyway*/
             std::cin >> RepeatAns;
             clearcin();
-            RepeatAns = toupper(RepeatAns); // We set the character to its uppercase equivalent, so we only get 2 condition checks instead of 4
+            RepeatAns = toupper(RepeatAns); /* We set the character to its uppercase equivalent, so we only get
+            2 condition checks instead of 4*/
             switch (RepeatAns) {
             case 'Y':
                 RepeatQuestion = false;
@@ -85,10 +90,11 @@ int main() {
 
     } while (RepeatGame == true);
 
-	return 0;
+    return 0;
 }
 
-// Function to clear out the buffer of cin, ensuring we don't take in more than one word and get weird inputs (or worse, infinite loops)
+/* Function to clear out the buffer of cin, ensuring we don't take in more than one word and get weird inputs
+(or worse, infinite loops)*/
 void clearcin() {
     std::cin.clear();
     std::cin.ignore(32767, '\n');
@@ -96,7 +102,8 @@ void clearcin() {
 
 // Function to initialize the gamestate, setting the characters back to 1-9 and resetting player and turn number
 void Init() {
-    //Initializing the board to 1-9, since char is just an integer we can simply add the value of '1' to it to start counting from there
+    /*Initializing the board to 1 - 9, since char is just an integer we can simply add the value of '1' to it
+    to start counting from there*/
     for (int i{ 0 }; i < rows; i++) {
         for (int j{ 0 }; j < cols; j++) {
             Board[i][j] = '1' + (i * 3) + j;
@@ -159,14 +166,16 @@ void PlayGame() {
             std::cout << "Player " << PlayerNumber << " has won the game! Congratulations!\n";
             return;
         }
-        PlayerNumber = 1 - PlayerNumber + 2; // Switch between player 1 and 2 every iteration of the loop (assuming PlayerNumber is set to either of the values already, which it is in Init)
+        PlayerNumber = 1 - PlayerNumber + 2; /* Switch between player 1 and 2 every iteration of the loop
+        (assuming PlayerNumber is set to either of the values already, which it is in Init)*/
         system("cls"); // Clear the screen
         DrawBoard(); // Draw the new game board
         TurnNumber += 1; // Increment the turn counter, so the game ends after 9 turns
     } while (TurnNumber != 10);
 }
 
-// Function to check for a winner of the game, it does this by checking if 3 grids in a row (diagonally, vertically, or horizontally) are the same
+/* Function to check for a winner of the game, it does this by checking if 3 grids in a row
+(diagonally, vertically, or horizontally) are the same*/
 bool CheckForWin() {
 
     // Check the 2 diagonals
@@ -177,51 +186,43 @@ bool CheckForWin() {
         return true;
     }
     // Check the 3 horizontals
-    else if (Board[0][0] == Board[0][1] && Board[0][1] == Board[0][2]) {
-        return true;
-    }
-    else if (Board[1][0] == Board[1][1] && Board[1][1] == Board[1][2]) {
-        return true;
-    }
-    else if (Board[2][0] == Board[2][1] && Board[2][1] == Board[2][2]) {
-        return true;
+    for (int i{}; i < rows; i++) {
+        if (Board[i][0] == Board[i][1] && Board[i][1] == Board[i][2]) {
+            return true;
+        }
     }
     // Check the 3 verticals
-    else if (Board[0][0] == Board[1][0] && Board[1][0] == Board[2][0]) {
-        return true;
-    }
-    else if (Board[0][1] == Board[1][1] && Board[1][1] == Board[2][1]) {
-        return true;
-    }
-    else if (Board[0][2] == Board[1][2] && Board[1][2] == Board[2][2]) {
-        return true;
+    for (int j{}; j < rows; j++) {
+        if (Board[0][j] == Board[1][j] && Board[1][j] == Board[2][j]) {
+            return true;
+        }
     }
     // If none of them are the same, return false as there is no winner yet
-    else return false;
+    return false;
 }
 
 // Function to play the game against an AI
 void PlayGameAI() {
-    
+
     int PlayerChoice{};
     int AIChoice{};
 
     std::cout << "Would you like to play as X or O?\n";
-    std::cin >> PlayerNumberChoiceCH;
+    std::cin >> PlayerCharacter;
     clearcin();
-    PlayerNumberChoiceCH = toupper(PlayerNumberChoiceCH); // Change the letter to uppercase so we only get 2 conditions instead of 4
+    PlayerCharacter = toupper(PlayerCharacter); // Change the letter to uppercase so we only get 2 conditions instead of 4
     // Check if they actually typed in either x or o
-    while (PlayerNumberChoiceCH != 'X' && PlayerNumberChoiceCH != 'O') {
+    while (PlayerCharacter != 'X' && PlayerCharacter != 'O') {
         std::cout << "That's not a valid answer! Please choose either 'X' or 'O'\n";
-        std::cin >> PlayerNumberChoiceCH;
+        std::cin >> PlayerCharacter;
         clearcin();
-        PlayerNumberChoiceCH = toupper(PlayerNumberChoiceCH);
+        PlayerCharacter = toupper(PlayerCharacter);
     }
     // Set PlayerNumberChoice according to which player they chose
-    if (PlayerNumberChoiceCH == 'X') {
+    if (PlayerCharacter == 'X') {
         PlayerNumberChoice = 1;
     }
-    else if (PlayerNumberChoiceCH == 'O') {
+    else if (PlayerCharacter == 'O') {
         PlayerNumberChoice = 2;
     }
     system("cls"); // Clean up all the initial text and stuff
